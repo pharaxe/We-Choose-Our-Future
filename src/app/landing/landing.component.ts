b94bcf467133a32c3a4a2dca7f2917222e714ee2
Grant Kaufmann <gkaufmann@pavlovmedia.com>
2018-09-29 18:36:29 -0500

    event tracker

--- src/app/landing/landing.component.ts
+++ src/app/landing/landing.component.ts
@@ -1,4 +1,5 @@
 import {Component, OnInit, OnDestroy} from '@angular/core';
+import {Election, Party} from './types/index'
 
 @Component({
   providers: [],
@@ -7,6 +8,40 @@ import {Component, OnInit, OnDestroy} from '@angular/core';
   styleUrls: ['./landing.component.css'],
 })
 export class LandingComponent implements OnInit, OnDestroy {
+  //  Democratic 	4	1
+  public events = [
+    {
+      label: 'event 1',
+      date: '1541509200000',
+      url: '.'
+    },
+    {
+      label: 'event 2',
+      date: '1541509200000',
+      url: '.'
+    },
+    {
+      label: 'event 3',
+      date: '1541509200000',
+      url: '.'
+    }
+
+  ];
+  public electionList: Election[] = [
+    {
+      name: 'Governor and Lieutenant Governor',
+      termLength: 4,
+      cards: [
+        {
+          name: 'JB Pritzker & Juliana Stratton',
+          pictureUrl: '../../assets/image/Governor/JB Pritzker.jpg',
+          websiteUrl: 'https://www.jbpritzker.com/',
+          party: Party.Democrat
+        }
+      ]
+    }
+  ];
+  //
   constructor() {}
   public ngOnInit() {}
   public ngOnDestroy() {}
--- src/app/landing/landing.module.ts
+++ src/app/landing/landing.module.ts
@@ -1,9 +1,13 @@
 import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
+import {EventTrackerModule} from './event-tracker/event-tracker.module'
+import {CandidateCardModule} from './candidate-card/candidate-card.module';
 import {LandingComponent} from './landing.component';
 
 @NgModule({
   imports: [
+    EventTrackerModule,
+    CandidateCardModule,
     CommonModule
   ],
   exports: [
