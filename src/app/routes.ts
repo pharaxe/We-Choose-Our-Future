2a18abc9f26d48bbbc69fcc3825ade86723b21df
Benjamin Sweedler <bsweedler@pavlovmedia.com>
2018-09-30 02:20:05 -0500

    making homepage truly a homepage

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -5,7 +5,7 @@ import {ContentComponent} from './content/content.component';
 export const appRoutes
 : Routes = [
   {path: 'landing', component: LandingComponent },
-  {path: 'homepage', component: ContentComponent },
-  {path: '', redirectTo: '', pathMatch: 'full'},
-  {path: '**', redirectTo: 'homepage', pathMatch: 'full'}
+  {path: '', component: ContentComponent },
+  {path: 'homepage', redirectTo: '', pathMatch: 'full'},
+  {path: '**', redirectTo: '', pathMatch: 'full'}
   ];
